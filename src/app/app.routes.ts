6eadac5c8436f901c264645a6ae3642ddf3033fc
Lucas <lucassoares.dev@hotmail.com>
2025-04-16 06:05:36 -0300

    Criando estruturas de pastas e inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/home.component').then(m => m.HomeComponent),
+  },
+  {
+    path: 'projetos',
+    loadComponent: () =>
+      import('./features/projetos/projetos.component').then(m => m.ProjetosComponent),
+  },
+  {
+    path: 'jogos',
+    loadComponent: () =>
+      import('./features/jogos/jogos.component').then(m => m.JogosComponent),
+  },
+  {
+    path: 'sobre',
+    loadComponent: () =>
+      import('./features/sobre/sobre.component').then(m => m.SobreComponent),
+  },
+  {
+    path: 'contato',
+    loadComponent: () =>
+      import('./features/contato/contato.component').then(m => m.ContatoComponent),
+  },
+];
